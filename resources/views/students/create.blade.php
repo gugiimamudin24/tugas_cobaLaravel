f9a89fc27c6fe72850a8dbf1680ad062ffdf2d4d
gugiimamudin24 <gugiimamudin@mail.unpas.ac.id>
2019-12-04 23:00:27 +0700

    menambahkan data mahasiswa

---
+++ resources/views/students/create.blade.php
@@ -0,0 +1,42 @@
+@extends('layout/main')
+
+@section('title', 'Form Tambah data Mahasiswa')
+
+@section('container')
+
+<div class="container">
+   <div class="row">
+    <div class="col-8">
+        <h1 class="mt-3">Form Tambah data Mahasiswa</h1>
+
+    <form method="post" action="/students">
+    @csrf
+        <div class="form-group">
+            <label for="nama">Nama</label>
+            <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" name="nama" placeholder="Masukan Nama">@error('nama')<div class="invalid-feedback">{{ $message}}</div>@enderror
+        </div>
+        <div class="form-group">
+            <label for="nrp">NRP</label>
+            <input type="text" class="form-control @error('nrp') is-invalid @enderror" id="nrp" name="nrp" placeholder="Masukan NRP">
+            @error('nrp')<div class="invalid-feedback">{{ $message}}</div>@enderror
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="Masukan Email">
+            @error('email')<div class="invalid-feedback">{{ $message}}</div>@enderror
+        </div>
+        <div class="form-group">
+            <label for="jurusan">Jurusan</label>
+            <input type="text" class="form-control @error('jurusan') is-invalid @enderror" id="jurusan" name="jurusan" placeholder="Masukan Jurusan">
+            @error('jurusan')<div class="invalid-feedback">{{ $message}}</div>@enderror
+        </div>
+
+        <button type="submit" class="btn btn-primary">Tambah Data</button>
+        
+    </form>
+      
+
+    </div>
+  </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 // root
 
 Route::get('/', 'PagesController@home');
 Route::get('/about', 'PagesController@about');
 
 Route::get('/mahasiswa', 'MahasiswaController@index');
+
+// Students
+Route::get('/students', 'StudentsController@index');
+Route::get('/students/create', 'StudentsController@create');
+Route::get('/students/{student}', 'StudentsController@show');
+Route::post('/students', 'StudentsController@store');
+
+
+
+
 
 
 
